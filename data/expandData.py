b3680cc03c34bde02acbb814c66235e824639463
Sheng Guo <enigmaguo@hotmail.com>
2012-01-19 11:37:56 -0800

    add more function on the expandData python code

--- data/expandData.py
+++ data/expandData.py
@@ -1,8 +1,10 @@
 import json
 import sys
-if len(sys.argv) < 3:
-	print("usage: input output max")
-else:
+
+if len(sys.argv) < 4:
+	print("usage 1: python expandData.py input output max")
+	print("usage 2: python expandData.py input output offset count")
+elif len(sys.argv) == 4:
 	inName = sys.argv[1]
 	outName = sys.argv[2]
 	max = int(sys.argv[3])
@@ -16,5 +18,18 @@ else:
 		modifiedLine = json.dumps(obj)
 		outfile.write(modifiedLine+"\n")
 		counter = counter+1
-
-
+elif len(sys.argv) == 5:
+	inName = sys.argv[1]
+	outName = sys.argv[2]
+	start = int(sys.argv[3])
+	max = int(sys.argv[4])+start
+	counter = start
+	outfile = open(outName,'w+')
+	lines = open(inName,'r').readlines();
+	while counter < max:
+		idx = counter% len(lines)
+		obj = json.loads(lines[idx])
+		obj['id'] = counter
+		modifiedLine = json.dumps(obj)
+		outfile.write(modifiedLine+"\n")
+		counter = counter+1
